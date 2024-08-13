99e0582f4f8df72da4ac826c5de213a5ea0dd807
Emil Piękoś <piekosemil@gmail.com>
2024-08-13 22:38:09 +0200

    Initial commit

---
+++ src/main/java/com/emilpiekos/contactwebsite/ContactWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.emilpiekos.contactwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactWebsiteApplication.class, args);
+	}
+
+}
